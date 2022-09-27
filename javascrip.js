7c0f6b5db4b5ccd6e149cd754fad5029f0c2a7f5
igor <igor@gmail.com>
2022-09-27 11:47:57 +0800

    add index.html home-diving-content

---
+++ javascrip.js
@@ -0,0 +1,13 @@
+
+
+// <!-- Initialize Swiper -->
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
